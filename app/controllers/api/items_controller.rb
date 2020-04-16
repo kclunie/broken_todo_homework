1edffb9e8105fbc59e4927b0299a2bd3b6b608a2
Kristen Clunie <kcluniesa@yahoo.com>
2020-04-15 23:08:38 -0400

    add index and show to items controller

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,9 +1,21 @@
 class Api::ItemsController < ApplicationController
 
+    before_action :set_project
+
     def index
         @items = Item.all
         render json: @items
-        
+    end
+
+    def show
+        @item = Item.find_by(id: params[:id])
+        render json: @item
+    end
+
+    private
+
+    def set_project
+        @project = Project.find(params[:project_id])
     end
 
 end
