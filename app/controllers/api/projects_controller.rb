df43dc01e6755f2eede51c4e946096f5196a2c6d
Kristen Clunie <kcluniesa@yahoo.com>
2020-04-15 20:54:23 -0400

    add projects api controller

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -2,8 +2,13 @@ class Api::ProjectsController < ApplicationController
 
     def index
         @projects = Project.all
-        render json: @projects
-        
+        render json: @projects   
+        # respond_to do |f|
+        #     f.html {render :index}
+        #     f.json {render json: @projects}
+        # end 
     end
+
+    
 
 end
