995fb7f079ada9bffb724c7801a68664539c574e
Kristen Clunie <kcluniesa@yahoo.com>
2020-04-17 15:13:36 -0400

    updated new items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -4,12 +4,27 @@ class Api::ItemsController < ApplicationController
 
     def index
         @items = Item.all
-        render json: @items
+        respond_to do |f|
+            f.html {render :index}
+            f.json {render json: @items}
+        end
     end
 
     def show
         @item = Item.find_by(id: params[:id])
         render json: @item
+    end
+
+    def new
+        @project = Project.find_by(id: params[:project_id])
+        if params[:project_id] && project = Project.find_by_id(params[:project_id])
+            @item = project.items.build 
+        else
+        @item = Item.new
+        end 
+    end
+
+    def create
     end
 
     private
