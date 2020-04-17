68569ec73a3490930ba8b9cecffd2ad79764035a
Kristen Clunie <kcluniesa@yahoo.com>
2020-04-16 22:51:05 -0400

    add edit action

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -2,7 +2,11 @@ class Api::ProjectsController < ApplicationController
 
     def index
         @projects = Project.all
-        render json: @projects   
+        respond_to do |f|
+            f.html {render :index}
+            f.json {render json: @projects}
+        end
+       # render json: @projects   
     end
 
     def show
@@ -10,5 +14,39 @@ class Api::ProjectsController < ApplicationController
         render json: @project
     end
     
+    def new
+        @project = Project.new
+    end
+
+    def create
+        @project = Project.new(project_params)
+        if @project.save
+            render json: @project
+        else
+         render :new
+        end 
+    end
+
+    def edit
+        @project = Project.find(params[:id])
+    end
+
+    def update
+        @project = Project.find(params[:id])
+        @project.update(project_params)
+        @project.save
+        render json: @project
+    end
+
+    def destroy
+        @project = Project.find(params[:id])
+        @project.destroy
+    end
+
+    private
+
+    def project_params
+        params.require(:project).permit(:title)
+    end
 
 end
