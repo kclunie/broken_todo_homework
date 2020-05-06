0a14fad1010229782bb1aa57d663cdf08be0abc4
Kristen Clunie <kcluniesa@yahoo.com>
2020-05-05 23:12:54 -0400

    test JSON data retrieval

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -2,6 +2,7 @@ class Api::ProjectsController < ApplicationController
 
     def index
         @projects = Project.all
+        #render json: @projects
         respond_to do |f|
             f.html {render :index}
             f.json {render json: @projects}
