0ee104b3f739d19e6664aaab0e63ad26057e6a13
Kristen Clunie <kcluniesa@yahoo.com>
2020-05-06 22:11:18 -0400

    add js submit function

--- app/assets/javascripts/api/projects.js
+++ app/assets/javascripts/api/projects.js
@@ -1,66 +1,66 @@
-$( document ).ready(function() {
-	console.log( "ready!" );
-	listenForClick()
-    listenForSubmitForm()
-});
+// $( document ).ready(function() {
+// 	console.log( "ready!" );
+// 	listenForClick()
+//     listenForSubmitForm()
+// });
 
-function listenForClick() {
-	$('button#events-data').on('click', function (event) {
-		event.preventDefault()
-		console.log("clicking projects show button")
-		getEvents()
-	})
-}
+// function listenForClick() {
+// 	$('button#projects-data').on('click', function (event) {
+// 		event.preventDefault()
+// 		console.log("clicking projects index button")
+// 		getProjects()
+// 	})
+// }
 
-function getEvents() {
-	console.log("in the getevents function")
-	$.ajax({
-		url: 'http://localhost:3000/api/projects',
-		method: 'get',
-		dataType: 'json'
-	}).done(function (data){
+// function getProjects() {
+// 	console.log("in the getprojects function")
+// 	$.ajax({
+// 		url: 'http://localhost:3000/api/projects',
+// 		method: 'get',
+// 		dataType: 'json'
+// 	}).done(function (data){
 
-			console.log("the data is: ", data)
-			data.map(event => {
-				const newEvent = new Project(event)
-				events.push(newEvent)
-				const newEventHtml = newEvent.projectHTML()
-				document.getElementById('ajax-events').innerHTML += newEventHtml
-				console.log(events)
-			})
-		})
-	}
+// 			console.log("the data is: ", data)
+// 			data.map(project => {
+// 				const newProject = new Project(project)
+// 				projects.push(newProject)
+// 				const newProjectHtml = newProject.projectHTML()
+// 				document.getElementById('ajax-projects').innerHTML += newProjectHtml
+// 				console.log(projects)
+// 			})
+// 		})
+// 	}
 
-	const events = []
+// 	const projects = []
 
-class Project {
-	constructor(obj) {
-		this.id = obj.id
-		this.title = obj.title
-	}
+// class Project {
+// 	constructor(obj) {
+// 		this.id = obj.id
+// 		this.title = obj.title
+// 	}
  
-}
+// }
 
-Project.prototype.projectHTML = function () {
-	return (`	
-		<div class='post'>
-			<h3>${this.title}</h3>
-		</div>
-	`)
-}
+// Project.prototype.projectHTML = function () {
+// 	return (`	
+// 		<div class='post'>
+// 			<h3>${this.title}</h3>
+// 		</div>
+// 	`)
+// }
 
-function listenForSubmitForm(){
-    $("#new_project").on("submit", function(e){
-		e.preventDefault()
-        console.log("submitting project")
-        const values = $(this).serialize()
-        console.log("values", values)
-		$.post("/projects", values).done(function(data){
-			console.log("data", data)
-			$("#new-project-container").html("")
-			const newProject = new Project(data)
-			const htmlToAdd = newProject.projectHTML()
-			$("#new-project-container").html(htmlToAdd)
-		})
-	})
-}
+// function listenForSubmitForm(){
+//     $("#new_project").on("submit", function(e){
+// 		e.preventDefault()
+//         console.log("submitting project")
+//         const values = $(this).serialize()
+//         console.log("values", values)
+// 		$.post("/api/projects", values).done(function(data){
+// 			console.log("data", data)
+// 			$("#new-project-container").html("")
+// 			const newProject = new Project(data)
+// 			const htmlToAdd = newProject.projectHTML()
+// 			$("#new-project-container").html(htmlToAdd)
+// 		})
+// 	})
+// }
